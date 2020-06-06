737a54ed206f0fa9bed05407e8b24283affe8ab1
Afraz Siddiqui <afsiddiq@microsoft.com>
2020-06-06 10:33:09 -0400

    Wrap up getting started

--- Messenger/Controllers/ConversationsViewController.swift
+++ Messenger/Controllers/ConversationsViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ConversationsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Messenger/Controllers/VideoPlayerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  VideoPlayerViewController.swift
+//  Messenger
+//
+//  Created by Afraz Siddiqui on 6/6/20.
+//  Copyright © 2020 ASN GROUP LLC. All rights reserved.
+//
+
+import UIKit
+
+class VideoPlayerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
