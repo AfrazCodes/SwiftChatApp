9ae0dc1093e37da5f797053232ce320b5ae46d8a
Afraz Siddiqui <afsiddiq@microsoft.com>
2020-06-06 10:24:15 -0400

    Getting Started

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Afraz Siddiqui on 6/6/20.
+//  Copyright © 2020 ASN GROUP LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
